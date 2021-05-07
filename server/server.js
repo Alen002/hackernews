9d2a5262e265fcacd8db710e0f6dd557d174f4d2
Alen <alechkr@gmail.com>
2021-05-07 20:00:11 +0200

    added PORT configuration to server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); 
+app.use(express.json());
 
-
-
-app.use(morgan('short'));  //short or combines
+app.use(morgan("short")); //short or combines
 /* app.use(express.static('./public/')); */
 
-app.use(bodyParser.urlencoded({extended: false})); // middleware - necessary to receive the inform. from data entered in the form
+app.use(bodyParser.urlencoded({ extended: false }));
 /* app.use(bodyParser.json()); */
 
+app.listen(PORT, () => {
+  console.log("Server is up and running on 5000");
+});
 
-app.listen(3000, () => {console.log('Server is up and running on 3000')});
+app.get("/", (req, res) => {
+  res.send("Main folder");
+});
 
-
-app.get('/', (req, res) => {
-    res.send('Main folder')    
+app.get("/webpage", (req, res) => {
+  res.send("This will fetch webpage");
 });
